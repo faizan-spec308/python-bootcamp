94577a80ca6b373eb344f5bbebf70eebc522906f
Faizan Naveed <faizan.business111@gmail.com>
2025-08-28 16:44:11 +0100

    Create Day1_basics.py

---
+++ Day1_basics.py
@@ -0,0 +1,14 @@
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+Height = input("Enter your height : ")
+
+print("Hi " + name + " You are " + str(age) + " years old" + " and your height is " + str(Height) + " Ft")
+
+
+
+
+
+
+
+
+
